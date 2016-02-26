316b7141964639865082d16cd54207ff4c488be0
Mel B <yopnopp@gmail.com>
2016-02-25 22:59:00 -0500

    Create Beverage.java
    See Coffee.java.

---
+++ Beverage.java
@@ -0,0 +1,17 @@
+class Beverage {
+	
+	public Beverage() {
+
+	}
+
+	public void isFull() {
+
+		System.out.println("No more coffee for you!");
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
